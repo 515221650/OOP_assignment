c276fead3e41366c1588d8709179f37c397fb411
杨卓毅 <yangzhuoyi@rayshaper.com>
2019-06-01 20:57:41 +0800

    update_con

---
+++ Layer/maxpool.h
@@ -0,0 +1,19 @@
+//
+// Created by yzy on 2019/6/1.
+//
+
+#ifndef OOP_MAXPOOL_H
+#define OOP_MAXPOOL_H
+
+#include "layer.h"
+
+class MaxPool : public Layer{
+private:
+    Layer* pre_layer;
+public:
+    MaxPool(int _in, int _out, Layer* pre, MyGraph& G): 
+
+};
+
+
+#endif //OOP_MAXPOOL_H
